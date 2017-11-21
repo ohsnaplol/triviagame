859f4ebff74190754bbea72d1fe30ce88fede0ff
ohsnaplol <emyb@bellsouth.net>
2017-11-21 16:39:16 -0500

    finished most logic, buggy timer Im bad at committing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,111 @@
-var points;
+var points = 0;
+var qIndex = -1;
+var response;
+var timerIsActive = false;
 
-$(document).ready(function functionName() {
-  for (var i = 0; i < questions.length; i++) {
-    showQuestion(questions[i]);
-    // wait x seconds
-    // add points
-  }
+$(document).ready(function () {
+  // BUTTONS
+  $("#startBtn").on("click", function () {
+    setupGameTimer();
+    showNextQuestion();
+    $("#startBtn").hide();
+  });
+  $("#choice1").on("click", function () {
+    response = 0;
+    checkResponse(0);
+  });
+  $("#choice2").on("click", function () {
+    response = 1;
+    checkResponse(1);
+  });
+  $("#choice3").on("click", function () {
+    response = 2;
+    checkResponse(2);
+  });
+  $("#choice4").on("click", function () {
+    response = 3;
+    checkResponse(3);
+  });
+  // setTimeout(function () {
+  //
+  // }, 2000)
+  // function myLoop() {
+  //   setTimeout(function () {
+  //     showQuestion(questions[qIndex]);
+  //     qIndex++
+  //   }, 2000)
+  //   if (qIndex < questions.length) {
+  //     myLoop();
+  //   }
+  // }
+  // myLoop();
+  // intervalId = setInterval(this.count(), 1000);
+  // for (var i = 0; i < questions.length; i++) {
+  //   qIndex = i;
+  //   setTimeout(function () {
+  //     showQuestion(questions[qIndex]);
+  //   }, 5000);
+  //   if (response == questions[qIndex].c) {
+  //     points++;
+  //     console.log("points: " + points);
+  //   }
+  // }
 })
+
+function showNextQuestion() {
+  qIndex++;
+  $("#questionLabel").html(questions[qIndex].q);
+  $("#choice1").html("A: " + questions[qIndex].a[0]);
+  $("#choice2").html("B: " + questions[qIndex].a[1]);
+  $("#choice3").html("C: " + questions[qIndex].a[2]);
+  $("#choice4").html("D: " + questions[qIndex].a[3]);
+}
+
+function setupGameTimer() {
+  var seconds = 9;
+  $("#timerLabel").html((seconds+1) + " left");
+  $("#timerLabel").show();
+  var x = setInterval(function () {
+    $("#timerLabel").html(seconds + " left");
+    seconds--;
+    if(seconds < 0) {
+      showResult(false);
+      clearInterval(x);
+    }
+  }, 1000);
+}
+
+function showResult(didWin) {
+  $("#timerLabel").hide();
+  $("#choice1").empty();
+  $("#choice2").empty();
+  $("#choice3").empty();
+  $("#choice4").empty();
+  if (didWin) {
+    points++;
+    $("#questionLabel").html("Congratulations! " + questions[qIndex].a[response] + " is correct!");
+    $("#pts").html("Points: " + points);
+  } else {
+    $("#questionLabel").html("The answer is actually " + questions[qIndex].a[questions[qIndex].c]);
+  }
+  if (qIndex >= questions.length) {
+    $("#questionLabel").append("GAME OVER");
+    $("#startBtn").show();
+  } else {
+    setTimeout(function () {
+      showNextQuestion();
+      setupGameTimer();
+    }, 5000);
+  }
+}
+
+function checkResponse(response) {
+  if (questions[qIndex].c == response) {
+    showResult(true);
+  } else {
+    showResult(false);
+  }
+}
 
 var questions = [
   {
@@ -21,11 +120,3 @@ var questions = [
     c: 3
   }
 ]
-
-function showQuestion(question) {
-  $("#questionLabel").html(question.q);
-  $("#choice1").html(question.a[0]);
-  $("#choice2").html(question.a[1]);
-  $("#choice3").html(question.a[2]);
-  $("#choice4").html(question.a[3]);
-}
